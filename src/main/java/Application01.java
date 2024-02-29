b6172039763d67b7e5c61a7f5c7323f704dde672
kenryu <jryu1521@gmail.com>
2024-02-29 16:19:35 +0900

    edit

---
+++ src/main/java/Application01.java
@@ -0,0 +1,46 @@
+public class Application01 {
+    public static void main(String[] args) {
+        // 스캐너로 데이터 입력
+
+
+
+
+    }
+
+
+    // 사칙연산
+
+
+    public static class Calculator{
+        public int add(int x, int y){
+
+            return 0;
+        }
+
+        public int subtract(int x, int y){
+
+            return 0;
+        }
+
+        public int multiply(int x, int y){
+            return 0;
+        }
+
+
+        public int division(int x, int y) throws IllegalArgumentException{
+
+            if(y == 0) throw new IllegalArgumentException();
+
+            return 0;
+
+        }
+        public int modulor(int x, int y) throws IllegalArgumentException{
+            return 0;
+        }
+
+    }
+
+
+
+}
+
