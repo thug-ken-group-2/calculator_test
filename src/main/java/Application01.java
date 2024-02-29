7bee08c0cb41be33b3ba86b2e9a8c0ba51a3026e
kenryu <jryu1521@gmail.com>
2024-02-29 16:06:01 +0900

    initial commit

---
+++ src/main/java/Application01.java
@@ -0,0 +1,42 @@
+public class Application01 {
+    public static void main(String[] args) {
+        // 스캐너로 데이터 입력
+
+
+
+
+    }
+
+
+    // 사칙연산
+
+
+    public static class Calculator{
+        public int add(int x, int y){
+
+            return 0;
+        }
+
+        public int subtract(int x, int y){
+
+            return 0;
+        }
+
+        public int multiply(int x, int y){
+            return 0;
+        }
+
+
+        public double division(int x, int y) throws IllegalArgumentException{
+
+
+            return 0;
+
+        }
+
+    }
+
+
+
+}
+
